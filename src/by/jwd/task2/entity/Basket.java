495f95ef523ec77185d30d7c58f84bab44ccdef2
maxiemar <denishicmihail@gmail.com>
2021-04-05 14:23:07 +0300

    Refactor
    * Replace several individual exceptions with
      IncompatibleStateException and
      InvalidArgumentException.
    * Add capacity and size.
    * Add contains() and containsAll().

--- src/by/jwd/task2/entity/Basket.java
+++ src/by/jwd/task2/entity/Basket.java
@@ -1,5 +1,8 @@
 package by.jwd.task2.entity;
 
+import by.jwd.task2.exception.IncompatibleStateException;
+import by.jwd.task2.exception.InvalidArgumentException;
+
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
@@ -9,9 +12,31 @@ import java.util.Set;
 public class Basket implements Serializable, Iterable<Ball> {
 
     private final Set<Ball> balls;
+    private int capacity;
+    private int size;
 
     public Basket() {
         balls = new HashSet<>();
+        capacity = 10;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) throws InvalidArgumentException, IncompatibleStateException {
+        if (capacity <= 0) {
+            throw new InvalidArgumentException("Capacity must be positive.");
+        }
+        if (capacity < size) {
+            throw new IncompatibleStateException("Cannot shrink basket. Capacity is less than current size.");
+        }
+        this.capacity = capacity;
     }
 
     // Returns an unmodifiable Set containing the balls.
@@ -19,55 +44,82 @@ public class Basket implements Serializable, Iterable<Ball> {
         return Set.copyOf(balls);
     }
 
-    public boolean putAll(Collection<Ball> balls) throws InvalidBallException {
+    public void putAll(Collection<Ball> balls) throws InvalidArgumentException, IncompatibleStateException {
         if (balls == null) {
-            throw new InvalidBallException("Ball collection cannot be null");
+            throw new InvalidArgumentException("Ball collection cannot be null");
         }
-        boolean changed = false;
         for (var ball : balls) {
-            if (put(ball)) {
-                changed = true;
-            }
+            put(ball);
         }
-        return changed;
     }
 
-    public boolean put(Ball ball) throws InvalidBallException {
+    public void put(Ball ball) throws InvalidArgumentException, IncompatibleStateException {
         if (ball == null) {
-            throw new InvalidBallException("Ball cannot be null");
+            throw new InvalidArgumentException("Ball cannot be null");
         }
-        boolean newBallPut = balls.add(ball);
-        return newBallPut;
+        if (size == capacity) {
+            throw new IncompatibleStateException("Basket is full.");
+        }
+        boolean newBallProvided = balls.add(ball);
+        if (newBallProvided) {
+            size++;
+        } else {
+            throw new IncompatibleStateException("Basket already contains this ball.");
+        }
     }
 
     public void clear() {
         balls.clear();
+        size = 0;
     }
 
-    public boolean removeAll(Collection<Ball> balls) throws InvalidBallException {
+    public void removeAll(Set<Ball> balls) throws InvalidArgumentException, IncompatibleStateException {
         if (balls == null) {
-            throw new InvalidBallException("Ball collection cannot be null");
+            throw new InvalidArgumentException("Ball set cannot be null");
         }
-        boolean changed = false;
         for (var ball : balls) {
-            if (remove(ball)) {
-                changed = true;
+            remove(ball);
+        }
+    }
+
+    public void remove(Ball ball) throws InvalidArgumentException, IncompatibleStateException {
+        if (ball == null) {
+            throw new InvalidArgumentException("Ball cannot be null");
+        }
+        boolean containedThisBall = balls.remove(ball);
+        if (containedThisBall) {
+            size--;
+        } else {
+            throw new IncompatibleStateException("Basket does not contain this ball.");
+        }
+    }
+
+    public boolean containsAll(Set<Ball> balls) throws InvalidArgumentException {
+        if (balls == null) {
+            throw new InvalidArgumentException("Ball set cannot be null");
+        }
+        for (var ball : balls) {
+            if (!contains(ball)) {
+                return false;
             }
         }
-        return changed;
+        return true;
     }
 
-    public boolean remove(Ball ball) throws InvalidBallException {
+    public boolean contains(Ball ball) throws InvalidArgumentException {
         if (ball == null) {
-            throw new InvalidBallException("Ball cannot be null");
+            throw new InvalidArgumentException("Ball cannot be null");
         }
-        boolean containedBall = balls.remove(ball);
-        return containedBall;
+        boolean containsThisBall = balls.contains(ball);
+        return containsThisBall;
     }
 
     @Override
     public int hashCode() {
-        return balls.hashCode() >>> 16;
+        int hashCode = balls.hashCode() >>> 16;
+        hashCode = 31 * hashCode + (capacity ^ (capacity >>> 16));
+        hashCode = 31 * hashCode + (size ^ (size >>> 16));
+        return hashCode;
     }
 
     @Override
@@ -79,12 +131,12 @@ public class Basket implements Serializable, Iterable<Ball> {
             return false;
         }
         Basket basket = (Basket) o;
-        return balls.equals(basket.balls);
+        return capacity == basket.capacity && size == basket.size && balls.equals(basket.balls);
     }
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + "@{" + "balls=" + balls + '}';
+        return getClass().getSimpleName() + "@{" + "balls=" + balls + ", capacity=" + capacity + ", size=" + size + '}';
     }
 
     @Override
